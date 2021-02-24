30fd5ed271d8d0fe3770ac72758134d068406334
arnaudMaillet <arnaud.maillet@saint-remi.net>
2021-02-24 02:42:01 +0100

    projet API météorem

---
+++ app/routes/appRoutes.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = function(app) {
+  var todoList = require('../controller/appController');
+
+  // todoList Routes
+  app.route('/stations')
+    .get(todoList.list_all_station)
+   
+  app.route('/stations/:stationName')
+    .get(todoList.read_a_station)
+
+  app.route('/stations/niveaux/:stationName')
+    .get(todoList.read_a_lvl)
+};
